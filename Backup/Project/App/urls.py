a527b627ced0c94e59af9d6724c435fbb09c1092
param-codezz <96417444+param-codezz@users.noreply.github.com>
2023-12-19 10:46:34 +0530

    updated backup

---
+++ Backup/Project/App/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from App import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('find', views.find, name='find'),
+    path('vacation', views.vacation, name='vacation'),
+    path('about', views.aboutUs, name='about'),
+    path('profile', views.profile, name='profile'),
+    path('login', views.login, name='login')
+]
--- Backup/Project/App/views.py
+++ Backup/Project/App/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def find(request):
+    return render(request, 'find.html')
+
+def vacation(request):
+    return HttpResponse('This is vacation page')
+
+def ourBrands(request):
+    return HttpResponse('This is our brands page')
+
+def aboutUs(request):
+    return HttpResponse('This is about us page')
+
+def profile(request):
+    return HttpResponse('This is profile page')
