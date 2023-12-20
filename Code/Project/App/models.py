4b34b051b2bbd4c273b565049fa1271b664ecbbf
The Boys <136tony.stark@gmail.com>
2023-12-20 16:06:46 +0530

    added model schema for GIGANIGGA

--- Code/Project/App/models.py
+++ Code/Project/App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# bimbimbambam
+
+
+# Krutik/Krutik
+# make user email (primary key)
+# user password
+# user Name
+# user hotels(max 10)
+# user membership status
+# user credit points
